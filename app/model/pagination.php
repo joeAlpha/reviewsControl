581bf4cc49a2bdd7a03cb2518d7145ff13951568
Jose Garcia <eng.josegc@gmail.com>
2020-08-05 16:51:57 -0500

    When the user request a new page of topics in review table, the new set returned starts from the right index

--- app/model/pagination.php
+++ app/model/pagination.php
@@ -28,71 +28,67 @@ per page values given by the controller of this API. -->
                 subject.user = '$userId' AND 
                 review.review_date <= curdate()";    
 
-        // Returns a mysql_result object
         $getTopicsResult = $connection->query($getTopicsQuery);
         
         if($getTopicsResult) {
-            // var_dump($getTopicsResult);
             $htmlContent = ""; // Table which contains the new topics
-            // $topics = [];
-            // while($topic = $getTopicsResult->fetch_array(MYSQLI_ASSOC)) $topics = $topic;
-            // var_dump(count($topics));
-
-            // echo "Ok ----- ";
             $limitFlag = 0; // For know how many topics was displayed
-            // $topic = $getTopicsResult->fetch_array(MYSQLI_BOTH);
-            // var_dump($topic);
-            // for($i = $indexBegin; $limitFlag <= $topicsPerPage && $i < $getTopicsResult->num_rows; $i++) {
+            
+            // Indicates when was reached the start index of the page requested,
+            // this helps to avoid show all topics starting from the first one.
+            $startFlag = 0; 
             while($topic = $getTopicsResult->fetch_array(MYSQLI_BOTH)) {
-                switch($topic['number_of_review']) {
-                    case 0: 
-                        $progressBarWidth = 'width: 10%'; 
-                        $progress = 1;
-                    break;
-                    case 1: 
-                        $progressBarWidth = 'width: 20%'; 
-                        $progress = 2;
-                    break;
-                    case 2: 
-                        $progressBarWidth = 'width: 30%'; 
-                        $progress = 3;
-                    break;
-                    case 3: 
-                        $progressBarWidth = 'width: 40%'; 
-                        $progress = 4;
-                    break;
-                    case 4: 
-                        $progressBarWidth = 'width: 50%'; 
-                        $progress = 5;
-                    break;
-                    case 5: 
-                        $progressBarWidth = 'width: 60%'; 
-                        $progress = 6;
-                    break;
-                    case 6: 
-                        $progressBarWidth = 'width: 70%'; 
-                        $progress = 7;
-                    break;
-                    case 7: 
-                        $progressBarWidth = 'width: 80%'; 
-                        $progress = 8;
-                    break;
-                    case 8: 
-                        $progressBarWidth = 'width: 90%'; 
-                        $progress = 9;
-                    break;
-                    case 9: 
-                        $progressBarWidth = 'width: 100%';
-                        $progress = 10;
-                     break;
-                } 
+                $startFlag++;
+                if($startFlag >= ($indexBegin - 1)) {
+                    switch($topic['number_of_review']) {
+                        case 0: 
+                            $progressBarWidth = 'width: 10%'; 
+                            $progress = 1;
+                        break;
+                        case 1: 
+                            $progressBarWidth = 'width: 20%'; 
+                            $progress = 2;
+                        break;
+                        case 2: 
+                            $progressBarWidth = 'width: 30%'; 
+                            $progress = 3;
+                        break;
+                        case 3: 
+                            $progressBarWidth = 'width: 40%'; 
+                            $progress = 4;
+                        break;
+                        case 4: 
+                            $progressBarWidth = 'width: 50%'; 
+                            $progress = 5;
+                        break;
+                        case 5: 
+                            $progressBarWidth = 'width: 60%'; 
+                            $progress = 6;
+                        break;
+                        case 6: 
+                            $progressBarWidth = 'width: 70%'; 
+                            $progress = 7;
+                        break;
+                        case 7: 
+                            $progressBarWidth = 'width: 80%'; 
+                            $progress = 8;
+                        break;
+                        case 8: 
+                            $progressBarWidth = 'width: 90%'; 
+                            $progress = 9;
+                        break;
+                        case 9: 
+                            $progressBarWidth = 'width: 100%';
+                            $progress = 10;
+                         break;
+                    } 
 
-                if(date('Y-m-d', strtotime($topic['review_date'])) >= date('Y-m-d')) $showRestoreButton = 'd-none';
-                else $showRestoreButton = '';
+                    if(date('Y-m-d', strtotime($topic['review_date'])) >= date('Y-m-d')) $showRestoreButton = 'd-none';
+                    else $showRestoreButton = '';
 
-                // Construction of each topic row
-                $htmlContent .= '
-                    <tr>
+                    // Construction of each topic row
+                    $htmlContent .= '
+                        <tr>
                         <td class="align-middle">' . $topic['name'] . '</td>
                         <td class="align-middle">' . '[subject name]' . '</td>
                         <td class="align-middle"> 
@@ -114,10 +110,11 @@ per page values given by the controller of this API. -->
                         'text-light mx-2 btn btn-lg btn-danger" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
                             <i class="fas fa-redo-alt mx-1"></i> Restore
                         </a>
-                    </td>' .
+                        </td>' .
 
-                    '</tr>';
-                $limitFlag++;
+                        '</tr>';
+                    $limitFlag++;
+                }
             }
             
             echo $htmlContent;
