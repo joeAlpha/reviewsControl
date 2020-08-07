12c2d7ba84ab4aa7c0d846af166d7993d331bbf3
Jose Garcia <eng.josegc@gmail.com>
2020-08-07 16:36:41 -0500

    Working on implement pagination on topic manager

--- app/view/reviewTable.php
+++ app/view/reviewTable.php
@@ -138,42 +138,4 @@
         </tbody>
     </table>
 
-    <!-- Pagination nav -->
-    <nav aria-label="Page navigation example" class="<?php if($reviewResult->num_rows <= $TOPICS_PER_PAGE) echo 'd-none'; ?>">
-  <ul class="pagination justify-content-center">
-    <li class="page-item disabled">
-      <a class="page-link mx-1 bg-dark border-0 rounded" href="#" tabindex="-1">
-        <i class="fas fa-chevron-left"></i>
-      </a>
-    </li>
-
-    <?php
-        $numberOfPages = ($reviewResult->num_rows / $TOPICS_PER_PAGE) + 1;
-        $allTopics = [];
-        while($row = $reviewResult->fetch_array(MYSQLI_ASSOC)) {
-            array_push($allTopics, $row);
-        }
-
-        for($i = 1; $i <= $numberOfPages; $i++) { ?>
-            <li class="page-item"> 
-                <a 
-                    class="page-link btn mx-1 bg-dark text-light border-0" 
-                    onclick="getPage(
-                        '<?php echo $TOPICS_PER_PAGE * $i - $TOPICS_PER_PAGE + 1; ?>',
-                        '<?php echo $_COOKIE['id']; ?>',
-                        '<?php echo $TOPICS_PER_PAGE; ?>'
-                    )" 
-                >
-                    <?php echo $i; ?>
-                </a>
-            </li>
-        <?php } ?>
-
-    <li class="page-item">
-      <a class="page-link mx-1 bg-dark border-0 text-light rounded" href="#">
-        <i class="fas fa-chevron-right"></i>
-      </a>
-    </li>
-  </ul>
-    </nav>
-
+<?php include("pagination.php"); ?>
